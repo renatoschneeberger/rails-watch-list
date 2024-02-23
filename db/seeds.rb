5c9c7f751ebfc1ea45011bea754c9155f05ae582
Renato Schneeberger <renato.schneeberger@iam-lab.ch>
2024-02-23 12:50:19 +0000

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,40 @@
 #   end
 
 # db/seeds.rb
+puts "destroy movie db"
+Bookmark.destroy_all
+Movie.destroy_all
+List.destroy_all
+
+
+puts "seed new Movie db"
+
 Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
 Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 Movie.create(title: "Wonder Woman 19842", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
 Movie.create(title: "The Shawshank Redemption2", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic2", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+movie1 = Movie.create(title: "Titanic2", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 Movie.create(title: "Ocean's Eight2", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+
+puts "destroy List db"
+puts movie1
+
+
+
+list1 = List.create(name: "All time favourites")
+List.create(name: "Shammi's top 10")
+List.create(name: "Matheus's picks")
+
+puts "seed new List db"
+puts list1
+
+
+bookmark1 = Bookmark.create(movie: movie1, list: list1, comment: "That worked")
+bookmark1 = Bookmark.create(movie: Movie.last, list: List.last, comment: "What a movie!!! Love it.")
+bookmark1 = Bookmark.create(movie: Movie.last, list: list1, comment: "What a movie!!! Love it.")
+
+
+print bookmark1
+puts " all done"
